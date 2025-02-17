543e9c4015a22c66fc1bee1ba702484f06925f0d
Anket Jadhav <anket.jadhav@scriptshub.net>
2025-02-17 12:46:52 +0530

    updated

--- src/test/java/pack1/MyClass.java
+++ src/test/java/pack1/MyClass.java
@@ -1,22 +1,28 @@
 package pack1;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 public class MyClass {
 
 	public static void main(String[] args) {
 
+		ChromeOptions options = new ChromeOptions();
+		
+		WebDriver driver = new ChromeDriver(options);
 
-	
-	
-	
-		
-		
-		
-		
-		
-		
-		
-		commit check
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
+		driver.manage().window().maximize();
+		try {
+		    driver.get("");
+		} catch (Exception e) {
+		    System.out.println("Error in accessing URL: " + e.getMessage());
+		    throw new RuntimeException("Test failed due to invalid URL");
+		}
 	}
 
 }
